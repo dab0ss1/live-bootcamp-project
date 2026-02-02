c5a29530873165f8f6435275e44cfe69ba1fa6ce
dab0ss1 <jonquantpfx@gmail.com>
2026-02-01 23:56:04 -0800

    Push dummy change to test workflow

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -10,8 +10,7 @@ use routes::*;
 // This struct encapsulates our application-related logic.
 pub struct Application {
     server: Serve<TcpListener, Router, Router>,
-    // address is exposed as a public field
-    // so we have access to it in tests.
+    // address is exposed as a public field so we have access to it in tests.
     pub address: String,
 }
 
